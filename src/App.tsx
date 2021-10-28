4f37369201f3ffd1bd7e8665a426ebbc24ed60ac
Ilya <IlyaDuginov1997@gmail.com>
2021-10-29 00:57:17 +0300

    Init comit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      Hello, it will be the best app for chekink beers!
+    </div>
+  );
+}
+
+export default App;
